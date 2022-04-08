5b5df8289e9364d38c6e6e93c7e64b1690043c2a
Robson Rosa <contatorobsonrosa@gmail.com>
2022-04-08 11:56:11 -0300

    create forms file

---
+++ frontend/src/components/forms.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Repositorio from "./repositorio";
+
+export default class Forms extends Repositorio {
+   
+    renderForms() {
+        return (
+            <div className="forms">
+                <h1 className="tittle">Add New Tool</h1>
+            </div>
+        )
+    }
+}
